8582ebbb3c5e56e8a771d674f00cfe37243f1128
Ilya Ivanov <ilya@devopsx.com>
2021-07-18 09:49:25 +0700

    typo

--- gha.sh
+++ gha.sh
@@ -6,4 +6,4 @@ if [[ "$INPUT_JENKINS_CERT" != 'false' ]]; then
   export REQUESTS_CA_BUNDLE="$INPUT_JENKINS_CERT"
 fi
 
-jenkins-jobs --conf "$INPUT_CONF_FILE" -p "$INPUT_TOKEN" update "$INPUT_JOBS_FILE"
+jenkins-jobs --conf "$INPUT_CONF_FILE" -p "$INPUT_JENKINS_TOKEN" update "$INPUT_JOBS_FILE"
