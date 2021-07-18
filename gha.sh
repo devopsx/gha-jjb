b04f8f7796837b2a5a61e11442821cf45816f8b0
Ilya Ivanov <ilya@devopsx.com>
2021-07-18 10:10:55 +0700

    fixed typo, removed debug

--- gha.sh
+++ gha.sh
@@ -2,13 +2,8 @@
 
 set -xeu -o pipefail
 
-pwd
-ls -la
-git status
-ls -la "$INPUT_CONF_FILE"
-
 if [[ "$INPUT_JENKINS_CERT" != 'false' ]]; then
   export REQUESTS_CA_BUNDLE="$INPUT_JENKINS_CERT"
 fi
 
-jenkins-jobs --conf "$INPUT_CONF_FILE" -p "$INPUT_JENKINS_TOKEN" update "$INPUT_JOBS_FILE"
+jenkins-jobs --conf "$INPUT_INI_FILE" -p "$INPUT_JENKINS_TOKEN" update "$INPUT_JOBS_FILE"
