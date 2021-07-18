aa5ce205db3f2def388f31b7f96778dbb2ec5efa
Ilya Ivanov <ilya@devopsx.com>
2021-07-18 09:35:35 +0700

    allow self-signed jenkins certs

--- gha.sh
+++ gha.sh
@@ -2,4 +2,8 @@
 
 set -xeu -o pipefail
 
+if [[ "$INPUT_JENKINS_CERT" != 'false' ]]; then
+  export REQUESTS_CA_BUNDLE="$INPUT_JENKINS_CERT"
+fi
+
 jenkins-jobs --conf "$INPUT_CONF_FILE" -p "$INPUT_TOKEN" update "$INPUT_JOBS_FILE"
