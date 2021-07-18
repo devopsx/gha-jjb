3e79f3ed7c41f9b3503f356ee3a8d5d9651c6e12
Ilya Ivanov <ilya@devopsx.com>
2021-07-18 19:24:09 +0700

    use a dir instead of a file

--- gha.sh
+++ gha.sh
@@ -6,4 +6,4 @@ if [[ "$INPUT_JENKINS_CERT" != 'false' ]]; then
   export REQUESTS_CA_BUNDLE="$INPUT_JENKINS_CERT"
 fi
 
-jenkins-jobs --conf "$INPUT_INI_FILE" -p "$INPUT_JENKINS_TOKEN" update "$INPUT_JOBS_FILE"
+jenkins-jobs --conf "$INPUT_INI_FILE" -p "$INPUT_JENKINS_TOKEN" update -r "$INPUT_JOBS_DIR"
