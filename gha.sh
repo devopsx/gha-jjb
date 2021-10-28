12b9d195e58a8f7a4a94e532f9b9e297b2674e28
Ilya Ivanov <ilya@devopsx.com>
2021-10-28 16:07:46 +0700

    added parameter to delete old jobs

--- gha.sh
+++ gha.sh
@@ -6,4 +6,9 @@ if [[ "$INPUT_JENKINS_CERT" != 'false' ]]; then
   export REQUESTS_CA_BUNDLE="$INPUT_JENKINS_CERT"
 fi
 
-jenkins-jobs --conf "$INPUT_INI_FILE" -p "$INPUT_JENKINS_TOKEN" update -r "$INPUT_JOBS_DIR"
+delete_old=""
+if [[ "$INPUT_DELETE_OLD" != 'false' ]]; then
+  delete_old="--delete-old"
+fi
+
+jenkins-jobs --conf "$INPUT_INI_FILE" -p "$INPUT_JENKINS_TOKEN" update -r "$INPUT_JOBS_DIR" $delete_old
